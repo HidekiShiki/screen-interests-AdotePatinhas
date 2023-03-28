40ac280aa13e9004385f638211e54e90de7cc59d
Hideki Kishi <jp.hidekifk@gmail.com>
2023-03-28 11:19:13 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Interests from './pages/Interests';
+
+function App() {
+  return (
+    <div className="App">
+      <Interests/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Interests/index.js
@@ -0,0 +1,15 @@
+import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+import './styles.css'
+
+function Interests () {
+    return (
+        <div className="interests">
+            <NavBar/>
+            hello word
+            <Footer/>
+        </div>
+    );
+}
+
+export default Interests
